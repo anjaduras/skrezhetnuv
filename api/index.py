9d320751ab4eaab8b1f17d5eac8c65e9f4410551
anjaduras <anna@duras.org>
2025-02-02 21:40:30 +0100

    basics covered

--- api/index.py
+++ api/index.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
 
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
