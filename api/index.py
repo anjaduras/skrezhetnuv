5c0a9831a3a6ea325f5d5b15f3de0d5a03b3c623
anjaduras <anna@duras.org>
2025-02-04 21:41:30 +0100

    ?

--- api/index.py
+++ api/index.py
@@ -13,5 +13,9 @@ def index():
 def contact():
     return render_template("contact.html")
 
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
